ac3b4a0810a9cc22b7337c7cdfa5b596584e874a
A. F. M. Mahbubur Rahman <m.r.kushal@gmail.com>
2024-09-21 01:07:15 +0600

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('page-content')
+    <br>
+    <br>
+    <p class="text-center">
+        <i class="bi bi-people-fill text-success" style="font-size: 5rem"></i>
+    </p>
+    <h1 class="text-center text-primary">Welcome to Employee information System</h1>
+@endsection
