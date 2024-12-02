a9165499333855bb2c1ade9b8733946f74afa54b
A. F. M. Mahbubur Rahman <m.r.kushal@gmail.com>
2024-12-02 14:50:39 +0600

    ui improvement

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
     <a class="navbar-brand" href="#"> <i class="bi bi-people-fill"></i> Employee Information System</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layout')
 
 @section('page-content')
-    <br>
-    <br>
-    <p class="text-center">
-        <i class="bi bi-people-fill text-success" style="font-size: 5rem"></i>
-    </p>
-    <h1 class="text-center text-primary">Welcome to Employee information System</h1>
+    <br><br>
+    <div class="card w-50 m-auto border-info rounded-lg">
+        <div class="card-body">
+            <p class="text-center">
+                <i class="bi bi-people-fill text-info" style="font-size: 5rem"></i>
+            </p>
+            <h1 class="text-center text-info">Employee <br>Information System</h1>
+        </div>
+    </div>
+
 @endsection
